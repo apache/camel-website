69046defd318b1072918007d7a6fb396a5397e9f
Zoran Regvart <zregvart@apache.org>
2018-12-14 21:51:48 +0100

    CAMEL-11492: add HTML minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,20 @@
-var gulp = require('gulp');
-var critical = require('critical').stream;
- 
-gulp.task('critical', function () {
+const gulp = require('gulp');
+const critical = require('critical').stream;
+const htmlmin = require('gulp-htmlmin');
+
+gulp.task('critical', () => {
   return gulp.src('public/**/index.html')
     .pipe(critical({base: 'public/', inline: true}))
+    .pipe(gulp.dest('public'));
+});
+
+gulp.task('minify', () => {
+  return gulp.src('public/**/*.html')
+    .pipe(htmlmin({
+      collapseBooleanAttributes: true,
+      collapseWhitespace: true,
+      collapseInlineTagWhitespace: true,
+      useShortDoctype: true
+    }))
     .pipe(gulp.dest('public'));
 });
