623b95645fd227f10e49dc28a76315105f1fb1b5
Zoran Regvart <zregvart@apache.org>
2019-04-20 16:20:55 +0200

    chore: generate the top level menu from hugo co...
    ...nfiguration
    
    With this a Handlebars helper, menu as defined in the `config.toml` for
    the Hugo generated portion of the site is parsed and a data structure is
    generated for the Handlebars template in the `antora-ui-camel` -- which
    makes the `config.toml` single place where the top level navigation menu
    is defined.

---
+++ menu.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+const toml = require('toml');
+const Handlebars = require('handlebars');
+
+const data = fs.readFileSync(path.join(__dirname, 'config.toml'), 'utf8');
+const hugoConfig = toml.parse(data);
+const mainMenu = hugoConfig.menu.main;
+
+const createMenu = item => {
+  return {
+    url: item.url || '#',
+    name: item.name,
+    children: mainMenu.filter(child => child.parent === item.identifier).map(createMenu)
+  }
+}
+
+const menuData = mainMenu.filter(item => typeof(item.parent) === 'undefined').map(createMenu);
+
+Handlebars.registerHelper('withMenuData', (options) => {
+  return options.fn({
+    items: menuData
+  });
+});
+
