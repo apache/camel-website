29e2d9dec4436b4bfa59f76e520e6fdf7389aa4c
Zoran Regvart <zregvart@apache.org>
2019-07-11 20:56:17 +0200

    chore: remove critical (from gulpfile.js)

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,5 @@
 const gulp = require('gulp');
-const critical = require('critical').stream;
 const htmlmin = require('gulp-htmlmin');
-
-gulp.task('critical', () => {
-  return gulp.src('public/**/index.html')
-    .pipe(critical({base: 'public/', inline: true}))
-    .pipe(gulp.dest('public'));
-});
 
 gulp.task('minify', () => {
   return gulp.src('public/**/*.html')
