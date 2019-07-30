c65066b628b59b1dcd8e45228259dfe9947f5ba0
Zoran Regvart <zregvart@apache.org>
2019-07-30 14:17:35 +0200

    chore: upgrade Antora default UI to latest
    This upgrades Antora default UI to the latest version and ports changes
    made to have the Camel branding to it. Antora default UI has moved a lot
    of the configuration into the variables and this should make it easier
    to maintain the Camel Antora UI based on it.
    
    Also removes the Serif font variants from use, somehow it didn't seem to
    be a good fit and deals with some visual quirks like the last dropdown
    menu being off screen.

--- antora-ui-camel/gulp.d/tasks/build.js
+++ antora-ui-camel/gulp.d/tasks/build.js
@@ -7,8 +7,8 @@ const concat = require('gulp-concat')
 const cssnano = require('cssnano')
 const fs = require('fs-extra')
 const imagemin = require('gulp-imagemin')
-const merge = require('merge-stream')
 const { obj: map } = require('through2')
+const merge = require('merge-stream')
 const ospath = require('path')
 const path = ospath.posix
 const postcss = require('gulp-postcss')
@@ -16,20 +16,20 @@ const postcssCalc = require('postcss-calc')
 const postcssImport = require('postcss-import')
 const postcssUrl = require('postcss-url')
 const postcssVar = require('postcss-custom-properties')
-const resolve = require('resolve')
 const uglify = require('gulp-uglify')
 const vfs = require('vinyl-fs')
 
 module.exports = (src, dest, preview) => () => {
   const opts = { base: src, cwd: src }
+  const sourcemaps = preview || process.env.SOURCEMAPS === 'true'
   const postcssPlugins = [
-    postcssImport(),
+    postcssImport,
     postcssUrl([
       {
         filter: '**/~typeface-*/files/*',
         url: (asset) => {
           const relpath = asset.pathname.substr(1)
-          const abspath = resolve.sync(relpath)
+          const abspath = require.resolve(relpath)
           const basename = ospath.basename(abspath)
           const destpath = ospath.join(dest, 'font', basename)
           if (!fs.pathExistsSync(destpath)) fs.copySync(abspath, destpath)
@@ -38,29 +38,39 @@ module.exports = (src, dest, preview) => () => {
       },
     ]),
     postcssVar({ preserve: preview ? 'preserve-computed' : false }),
-    postcssCalc(),
-    autoprefixer({ browsers: ['last 2 versions'] }),
+    preview ? postcssCalc : () => {},
+    autoprefixer,
     preview ? () => {} : cssnano({ preset: 'default' }),
   ]
 
   return merge(
     vfs
-      .src('js/+([0-9])-*.js', opts)
+      .src('js/+([0-9])-*.js', { ...opts, sourcemaps })
       .pipe(uglify())
       .pipe(concat('js/site.js')),
     vfs
-      .src('js/vendor/*.js', Object.assign({ read: false }, opts))
+      .src('js/vendor/*.js', { ...opts, read: false })
       .pipe(
         // see https://gulpjs.org/recipes/browserify-multiple-destination.html
         map((file, enc, next) => {
-          file.contents = browserify(file.relative, { basedir: src, detectGlobals: false }).bundle()
-          next(null, file)
+          if (file.relative.endsWith('.bundle.js')) {
+            file.contents = browserify(file.relative, { basedir: src, detectGlobals: false })
+              .plugin('browser-pack-flat/plugin')
+              .bundle()
+            file.path = file.path.slice(0, file.path.length - 10) + '.js'
+            next(null, file)
+          } else {
+            fs.readFile(file.path, 'UTF-8').then((contents) => {
+              file.contents = Buffer.from(contents)
+              next(null, file)
+            })
+          }
         })
       )
       .pipe(buffer())
       .pipe(uglify()),
-    vfs.src('css/site.css', opts).pipe(postcss(postcssPlugins)),
-    vfs.src('font/*.woff*(2)', opts),
+    vfs.src('css/site.css', { ...opts, sourcemaps }).pipe(postcss(postcssPlugins)),
+    vfs.src('font/*.{ttf,woff*(2)}', opts),
     vfs
       .src('img/**/*.{jpg,ico,png,svg}', opts)
       .pipe(
@@ -74,5 +84,5 @@ module.exports = (src, dest, preview) => () => {
     vfs.src('helpers/*.js', opts),
     vfs.src('layouts/*.hbs', opts),
     vfs.src('partials/*.hbs', opts)
-  ).pipe(vfs.dest(dest))
+  ).pipe(vfs.dest(dest, { sourcemaps: sourcemaps && '.' }))
 }
