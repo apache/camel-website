30fa68061505779942108310f125da6b4da34285
Zoran Regvart <zregvart@apache.org>
2019-08-15 23:16:10 +0200

    CAMEL-13622: filter box for component reference
    This adds a filter box for the component reference documentation, it's
    not enabled for other components as it is quite trivial and would break
    nested navigation -- which is not used in the component reference.

---
+++ antora-ui-camel/src/js/05-nav-search.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var tokenize = function (c) {
+    return '.*' + c + '.*'
+  }
+  var navSearch = document.querySelector('.nav-panel-menu input.search')
+  if (!navSearch) {
+    return
+  }
+
+  var navLinks = document.querySelectorAll('.nav-link')
+  navSearch.addEventListener('keyup', function () {
+    var tokens = navSearch.value.split('')
+    var term = new RegExp(tokens.map(tokenize).join(''), 'i')
+    for (var i = 0; i < navLinks.length; i++) {
+      var text = navLinks[i].textContent
+      var matches = term.test(text)
+      var replacement = ''
+      if (matches) {
+        navLinks[i].style.display = ''
+        if (tokens.length === 0) {
+          replacement = text
+        } else {
+          var prev = 0
+          var curr = 0
+          for (var j = 0; j < tokens.length; j++) {
+            curr = text.toLowerCase().indexOf(tokens[j].toLowerCase(), curr)
+            replacement = replacement + text.substring(prev, curr) + '<b>' + text[curr] + '</b>'
+            curr++
+            prev = curr
+          }
+          if (curr < text.length) {
+            replacement = replacement + text.substring(curr)
+          }
+        }
+      } else {
+        navLinks[i].style.display = 'none'
+        replacement = text
+      }
+      navLinks[i].innerHTML = replacement
+    }
+  })
+})
