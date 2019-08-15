1a42d8a4f32759adc3b0d02da27062c0576a9c26
nayananga@nayananga-Aspire-E5-575G <nayanangamuhandiram@gmail.com>
2019-08-15 14:37:22 +0530

    add: add search for hugo

---
+++ static/js/search.js
@@ -0,0 +1,99 @@
+/* global $ instantsearch algoliasearch */
+
+const searchClient = algoliasearch(
+    'YOUR_APP_ID',
+    'SEARCH_ONLY_API_KEY'
+  );
+  
+  const autocomplete = instantsearch.connectors.connectAutocomplete(
+    ({ indices, refine, widgetParams }, isFirstRendering) => {
+      const { container, onSelectChange } = widgetParams;
+  
+      if (isFirstRendering) {
+        container.html('<select id="ais-autocomplete"></select>');
+  
+        container.find('select').selectize({
+          options: [],
+          valueField: 'title',
+          labelField: 'title',
+          highlight: false,
+          onType: refine,
+          onBlur() {
+            refine(this.getValue());
+          },
+          score() {
+            return function() {
+              return 1;
+            };
+          },
+          onChange(value) {
+            onSelectChange(value);
+            refine(value);
+          },
+        });
+  
+        return;
+      }
+  
+      const [select] = container.find('select');
+  
+      indices.forEach(index => {
+        select.selectize.clearOptions();
+        index.results.hits.forEach(h => select.selectize.addOption(h));
+        select.selectize.refreshOptions(select.selectize.isOpen);
+      });
+    }
+  );
+  
+  const suggestions = instantsearch({
+    indexName: 'Apache Camel-website',
+    searchClient,
+  });
+  
+  suggestions.addWidget(
+    instantsearch.widgets.configure({
+      hitsPerPage: 5,
+    })
+  );
+  
+  suggestions.addWidget(
+    autocomplete({
+      container: $('#autocomplete'),
+      onSelectChange(value) {
+        search.helper.setQuery(value).search();
+      },
+    })
+  );
+  
+  const search = instantsearch({
+    indexName: 'Apache Camel-website',
+    searchClient,
+  });
+  
+  search.addWidget(
+    instantsearch.widgets.configure({
+      hitsPerPage: 10,
+    })
+  );
+  
+  // search.addWidget(
+  //   instantsearch.widgets.hits({
+  //     container: '#hits',
+  //     templates: {
+  //       item: `
+  //         <div>
+  //           <header class="hit-name">
+  //             {{#helpers.highlight}}{ "attribute": "title" }{{/helpers.highlight}}
+  //           </header>
+  //           <p class="hit-description">
+  //             {{#helpers.highlight}}{ "attribute": "description" }{{/helpers.highlight}}
+  //           </p>
+  //         </div>
+  //       `,
+  //     },
+  //   })
+  // );
+  
+  suggestions.start();
+  search.start();
+  
