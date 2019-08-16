db00551eb02566f01db20c8a30d1b3c0089dd22c
nayananga@nayananga-Aspire-E5-575G <nayanangamuhandiram@gmail.com>
2019-08-16 11:15:18 +0530

    add: add search to antora and recommended fixes done

--- antora-ui-camel/src/js/05-search.js
+++ antora-ui-camel/src/js/05-search.js
@@ -1,16 +1,16 @@
 /* global $ instantsearch algoliasearch */
 
 const searchClient = algoliasearch(
-  'YOUR_APP_ID',
-  'SEARCH_ONLY_API_KEY'
-);
+  'NF2UGSG10Y',
+  'cb696d6060df9f04ee84c4f6e15fd3b4'
+)
 
 const autocomplete = instantsearch.connectors.connectAutocomplete(
   ({ indices, refine, widgetParams }, isFirstRendering) => {
-    const { container, onSelectChange } = widgetParams;
+    const { container, onSelectChange } = widgetParams
 
     if (isFirstRendering) {
-      container.html('<select id="ais-autocomplete"></select>');
+      container.html('<select id="ais-autocomplete"></select>')
 
       container.find('select').selectize({
         options: [],
@@ -18,63 +18,63 @@ const autocomplete = instantsearch.connectors.connectAutocomplete(
         labelField: 'title',
         highlight: false,
         onType: refine,
-        onBlur() {
-          refine(this.getValue());
+        onBlur () {
+          refine(this.getValue())
         },
-        score() {
-          return function() {
-            return 1;
-          };
+        score () {
+          return function () {
+            return 1
+          }
         },
-        onChange(value) {
-          onSelectChange(value);
-          refine(value);
+        onChange (value) {
+          onSelectChange(value)
+          refine(value)
         },
-      });
+      })
 
-      return;
+      return
     }
 
-    const [select] = container.find('select');
+    const [select] = container.find('select')
 
-    indices.forEach(index => {
-      select.selectize.clearOptions();
-      index.results.hits.forEach(h => select.selectize.addOption(h));
-      select.selectize.refreshOptions(select.selectize.isOpen);
-    });
+    indices.forEach((index) => {
+      select.selectize.clearOptions()
+      index.results.hits.forEach((h) => select.selectize.addOption(h))
+      select.selectize.refreshOptions(select.selectize.isOpen)
+    })
   }
-);
+)
 
 const suggestions = instantsearch({
   indexName: 'Apache Camel-website',
   searchClient,
-});
+})
 
 suggestions.addWidget(
   instantsearch.widgets.configure({
     hitsPerPage: 5,
   })
-);
+)
 
 suggestions.addWidget(
   autocomplete({
     container: $('#autocomplete'),
-    onSelectChange(value) {
-      search.helper.setQuery(value).search();
+    onSelectChange (value) {
+      search.helper.setQuery(value).search()
     },
   })
-);
+)
 
 const search = instantsearch({
   indexName: 'Apache Camel-website',
   searchClient,
-});
+})
 
 search.addWidget(
   instantsearch.widgets.configure({
     hitsPerPage: 10,
   })
-);
+)
 
 // search.addWidget(
 //   instantsearch.widgets.hits({
@@ -94,5 +94,5 @@ search.addWidget(
 //   })
 // );
 
-suggestions.start();
-search.start();
+suggestions.start()
+search.start()
