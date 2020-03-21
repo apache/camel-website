48cf9c156f87c2bdcb7050139b0f88f5e6d02d3f
aashnajena <jenaaashna@gmail.com>
2020-03-21 15:10:49 +0530

    made changes for device width

--- antora-ui-camel/src/js/04-mobile-navbar.js
+++ antora-ui-camel/src/js/04-mobile-navbar.js
@@ -11,13 +11,18 @@ document.addEventListener('DOMContentLoaded', function () {
   })
 
   var menuDropDowns = Array.prototype.slice.call(document.querySelectorAll('.has-dropdown'), 0)
+  var maxMobileWidth = 1023
   menuDropDowns.forEach(function (el) {
     el.addEventListener('click', function (e) {
       var dropDownMenu = el.querySelectorAll('.navbar-dropdown')[0]
-      if (dropDownMenu.style.display === 'block') {
-        dropDownMenu.style.display = 'none'
+      if (window.innerWidth <= maxMobileWidth) {
+        if (dropDownMenu.style.display === 'block') {
+          dropDownMenu.style.display = 'none'
+        } else {
+          dropDownMenu.style.display = 'block'
+        }
       } else {
-        dropDownMenu.style.display = 'block'
+        dropDownMenu.style.display = 'none'
       }
     })
   })
