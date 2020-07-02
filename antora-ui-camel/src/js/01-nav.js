93f27e41ff832d563111c26d2f1af3f1d55cdf75
Aemie Jariwala <44139348+AemieJ@users.noreply.github.com>
2020-07-02 14:25:25 +0530

    fix: nav panel explore toggling on click

--- antora-ui-camel/src/js/01-nav.js
+++ antora-ui-camel/src/js/01-nav.js
@@ -38,11 +38,11 @@
     }
   })
 
-  nav.querySelector('.context').addEventListener('click', function () {
+  nav.querySelector('.nav-panel-explore').addEventListener('click', function () {
     var currentPanel = nav.querySelector('.is-active[data-panel]')
-    var activatePanel = currentPanel.dataset.panel === 'menu' ? 'explore' : 'menu'
-    currentPanel.classList.toggle('is-active')
-    nav.querySelector('[data-panel=' + activatePanel + ']').classList.toggle('is-active')
+    var activatePanel = (currentPanel === null || currentPanel.dataset.panel === 'menu') ? 'explore' : 'menu'
+    nav.querySelector(`[data-panel=${activatePanel}]`).classList.toggle('is-active')
+    if (currentPanel !== null) currentPanel.classList.toggle('is-active')
   })
 
   // NOTE prevent text from being selected by double click
