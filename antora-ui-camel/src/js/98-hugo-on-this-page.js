37a30fa88323dba03c3b1821c3ef3869a43c23d6
Aemie <aemie.j@hotmail.co.uk>
2020-08-08 00:01:42 +0530

    CAMEL-15337: update the logic on scroll based highlight

--- antora-ui-camel/src/js/98-hugo-on-this-page.js
+++ antora-ui-camel/src/js/98-hugo-on-this-page.js
@@ -4,46 +4,55 @@
   var headings = Array.from(document.querySelectorAll('article.doc h1, article.doc h2, article.doc h3'))
   var links = Array.from(document.querySelectorAll('aside.toc-sidebar .toc-hugo a'))
   var selectorRange = document.querySelector('.toolbar.hugo')
-  var minRange = selectorRange.getBoundingClientRect().top + selectorRange.getBoundingClientRect().height
-  var maxRange = minRange + 20
-  var prevLink = null
-  var activeFragment = null
+  if (selectorRange) {
+    var minRange = selectorRange.getBoundingClientRect().top + selectorRange.getBoundingClientRect().height - 5
+    var maxRange = minRange + 25
+    var prevLink = null
+    var activeFragment = null
+    var fragmentIndex
 
-  headings.some((heading, idx) => {
-    if (heading.innerText === 'Contents') {
-      headings.splice(idx, 1)
-      return true
-    }
-  })
-
-  links.forEach((link) => {
-    link.addEventListener('click', function (e) {
-      if (prevLink !== null && prevLink !== link) prevLink.classList.remove('is-active')
-      e.stopPropagation()
-      link.classList.add('is-active')
-      prevLink = link
-    })
-  })
-
-  window.addEventListener('load', function () {
-    onscroll()
-    window.addEventListener('scroll', onscroll)
-  })
-
-  function onscroll () {
-    headings.some((heading) => {
-      var topVal = heading.getBoundingClientRect().top
-      if (topVal >= minRange && topVal <= maxRange) {
-        activeFragment = heading.id
+    headings.some((heading, idx) => {
+      if (heading.innerText === 'Contents') {
+        headings.splice(idx, 1)
         return true
       }
     })
 
-    links.forEach((link) => {
-      if (activeFragment !== null && link.href.toString().split('#')[1] === activeFragment) {
+    window.addEventListener('load', function () {
+      onscroll()
+      window.addEventListener('scroll', onscroll)
+    })
+  }
+
+  function onscroll () {
+    headings.some((heading, idx) => {
+      var topVal = Math.ceil(heading.getBoundingClientRect().top)
+      if (topVal >= minRange && topVal <= maxRange) {
+        activeFragment = heading
+        fragmentIndex = idx
+        return true
+      }
+      if (activeFragment !== null && topVal < minRange && topVal > activeFragment.getBoundingClientRect().top) {
+        activeFragment = heading
+        fragmentIndex = idx
+      }
+    })
+
+    if (activeFragment !== null && Math.ceil(headings[fragmentIndex].getBoundingClientRect().top) > maxRange) {
+      --fragmentIndex
+      activeFragment = headings[fragmentIndex]
+      if (fragmentIndex === -1) {
+        activeFragment = null
+      }
+    }
+    links.some((link) => {
+      if (activeFragment != null && activeFragment.id === link.href.toString().split('#')[1]) {
         if (prevLink !== null && prevLink !== link) prevLink.classList.remove('is-active')
         link.classList.add('is-active')
         prevLink = link
+      }
+      if ((activeFragment === null || fragmentIndex === 0) && prevLink !== null) {
+        prevLink.classList.remove('is-active')
       }
     })
   }
