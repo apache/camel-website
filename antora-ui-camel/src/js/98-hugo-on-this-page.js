74d221cfc11b19d366757336d9bf7be1781aaa0c
Aemie <aemie.j@hotmail.co.uk>
2020-08-07 10:16:17 +0530

    CAMEL-15337: modify implementation on fragment jumping

--- antora-ui-camel/src/js/98-hugo-on-this-page.js
+++ antora-ui-camel/src/js/98-hugo-on-this-page.js
@@ -1,21 +1,13 @@
 ;(function () {
   'use strict'
 
-  var parent
-  var child = []
-  var lastActiveLink = null
-  var selector = document.querySelector('.toolbar')
   var headings = Array.from(document.querySelectorAll('article.doc h1, article.doc h2, article.doc h3'))
   var links = Array.from(document.querySelectorAll('aside.toc-sidebar .toc-hugo a'))
-  var width = document.querySelector('.navbar').offsetWidth
-  var parentSelector = document.querySelectorAll('.toc-hugo li ul:first-child')
-
-  var topValMin = selector.getBoundingClientRect().top + selector.getBoundingClientRect().height - 5
-  var topValMax = topValMin + 20
-  var lastActiveFragment = headings[0]
-  var activeFragment = lastActiveFragment
-  var detectedFragment = false
-  var fragmentIndex = 0
+  var selectorRange = document.querySelector('.toolbar.hugo')
+  var minRange = selectorRange.getBoundingClientRect().top + selectorRange.getBoundingClientRect().height
+  var maxRange = minRange + 20
+  var prevLink = null
+  var activeFragment = null
 
   headings.some((heading, idx) => {
     if (heading.innerText === 'Contents') {
@@ -23,69 +15,36 @@
       return true
     }
   })
-  createDataLevel()
-  function createDataLevel () {
-    if (parentSelector) {
-      parent = (width < 1024) ? Array.from(parentSelector[0].children) : Array.from(parentSelector[1].children)
-      parent.forEach(function (subParent) {
-        var subParentSelector = subParent.querySelector('ul')
-        if (subParentSelector) child = Array.from(subParentSelector.children)
-        if (child.length >= 1) {
-          child.forEach(function (base) {
-            base.setAttribute('data-level', '2')
-          })
-        }
-      })
-    }
-  }
 
-  window.addEventListener('load', function () {
-    onScroll()
-    window.addEventListener('scroll', onScroll)
+  links.forEach((link) => {
+    link.addEventListener('click', function (e) {
+      if (prevLink !== null && prevLink !== link) prevLink.classList.remove('is-active')
+      e.stopPropagation()
+      link.classList.add('is-active')
+      prevLink = link
+    })
   })
 
-  function onScroll () {
-    headings.some((heading, idx) => {
-      detectedFragment = false
-      var topVal = Math.ceil(heading.getBoundingClientRect().top)
-      if (topVal >= topValMin && topVal <= topValMax) {
-        detectedFragment = true
-        lastActiveFragment = activeFragment
-        activeFragment = heading
-        fragmentIndex = idx
+  window.addEventListener('load', function () {
+    onscroll()
+    window.addEventListener('scroll', onscroll)
+  })
+
+  function onscroll () {
+    headings.some((heading) => {
+      var topVal = heading.getBoundingClientRect().top
+      if (topVal >= minRange && topVal <= maxRange) {
+        activeFragment = heading.id
         return true
       }
     })
-    if (!detectedFragment) {
-      if (Math.ceil(headings[fragmentIndex].getBoundingClientRect().top) > topValMax) {
-        if (fragmentIndex !== 0) {
-          lastActiveFragment = headings[fragmentIndex]
-          --fragmentIndex
-          activeFragment = headings[fragmentIndex]
-        }
-        fragmentIndex = headings.indexOf(activeFragment)
-      } else {
-        lastActiveFragment = activeFragment
+
+    links.forEach((link) => {
+      if (activeFragment !== null && link.href.toString().split('#')[1] === activeFragment) {
+        if (prevLink !== null && prevLink !== link) prevLink.classList.remove('is-active')
+        link.classList.add('is-active')
+        prevLink = link
       }
-    }
-    if (window.pageYOffset + window.innerHeight + 2 >= document.documentElement.scrollHeight) {
-      ++fragmentIndex
-      lastActiveFragment = activeFragment
-      activeFragment = headings[fragmentIndex]
-    }
-    if (fragmentIndex !== 0) {
-      links.some((link) => {
-        var linkId = link.href.toString().split('#')[1]
-        if (activeFragment.id === linkId) {
-          if (lastActiveLink !== null && lastActiveLink !== link) lastActiveLink.classList.remove('is-active')
-          lastActiveLink = link
-          link.classList.add('is-active')
-          return true
-        }
-      })
-    }
-    if (fragmentIndex === 0 && lastActiveLink !== null) {
-      lastActiveLink.classList.remove('is-active')
-    }
+    })
   }
 })()
