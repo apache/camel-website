apache/camel-website

commit fb75497471e8975238ed6114e3550e01f02c533f

CAMEL-15948: rework camel-quarkus-examples
This adds the `@djencks/asciidoctor-jsonpath` dependency and a `exclude`
helper to the theme so we can exclude `Camel Quarkus Examples` component
from the navigation.

antora-ui-camel/src/helpers/exclude.js
@@ -1,0 +1,7 @@
+'use strict'
+
+module.exports = (elements, del) => {
+  const ary = Object.entries(elements)
+  const idx = ary.indexOf(del)
+  return ary.splice(idx, 1)
+}

antora-ui-camel/src/partials/nav-explore.hbs
@@ -6,7 +6,7 @@
   </div>
   {{/if}}
   <ul class="components">
-    {{#each (order_components site.components)}}
+    {{#each (exclude (order_components site.components) 'Camel Quarkus examples')}}
     <li class="component{{#if (eq this @root.page.component)}} is-current{{/if}}">
       <span class="title">{{{./title}}}</span>
       <ul class="versions">

.pnp.js
@@ -51,7 +51,8 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@antora/site-generator-default", "npm:3.0.0-alpha.1"],
             ["@antora/site-mapper", "npm:3.0.0-alpha.1"],
             ["@antora/xref-validator", "https://gitlab.com/antora/xref-validator.git#commit=19bfaf88fc7dd39552cfd8b3ac41c55d6d8f9edd"],
-            ["@djencks/asciidoctor-antora-indexer", "npm:0.0.5"],
+            ["@djencks/asciidoctor-antora-indexer", "npm:0.0.6"],
+            ["@djencks/asciidoctor-jsonpath", "npm:0.0.1"],
             ["@zregvart/cargo-deadlinks", "github-release:deadlinks/cargo-deadlinks:0.6.1/deadlinks-linux"],
             ["antora-ui-camel", "workspace:antora-ui-camel"],
             ["del", "npm:6.0.0"],
@@ -671,13 +672,25 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         }]
       ]],
       ["@djencks/asciidoctor-antora-indexer", [
-        ["npm:0.0.5", {
-          "packageLocation": "./.yarn/cache/@djencks-asciidoctor-antora-indexer-npm-0.0.5-df7634ac68-1844873453.zip/node_modules/@djencks/asciidoctor-antora-indexer/",
+        ["npm:0.0.6", {
+          "packageLocation": "./.yarn/cache/@djencks-asciidoctor-antora-indexer-npm-0.0.6-4db3f3a720-70a23e1885.zip/node_modules/@djencks/asciidoctor-antora-indexer/",
           "packageDependencies": [
-            ["@djencks/asciidoctor-antora-indexer", "npm:0.0.5"],
+            ["@djencks/asciidoctor-antora-indexer", "npm:0.0.6"],
             ["camelcase-keys", "npm:6.2.2"],
             ["esprima", "npm:4.0.1"],
             ["picomatch", "npm:2.1.1"],
+            ["static-eval", "npm:2.1.0"]
+          ],
+          "linkType": "HARD",
+        }]
+      ]],
+      ["@djencks/asciidoctor-jsonpath", [
+        ["npm:0.0.1", {
+          "packageLocation": "./.yarn/cache/@djencks-asciidoctor-jsonpath-npm-0.0.1-6ad9bf8c33-55493d1df5.zip/node_modules/@djencks/asciidoctor-jsonpath/",
+          "packageDependencies": [
+            ["@djencks/asciidoctor-jsonpath", "npm:0.0.1"],
+            ["esprima", "npm:4.0.1"],
+            ["jsonpath", "npm:1.0.2"],
             ["static-eval", "npm:2.1.0"]
           ],
           "linkType": "HARD",
@@ -1494,7 +1507,8 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@antora/site-generator-default", "npm:3.0.0-alpha.1"],
             ["@antora/site-mapper", "npm:3.0.0-alpha.1"],
             ["@antora/xref-validator", "https://gitlab.com/antora/xref-validator.git#commit=19bfaf88fc7dd39552cfd8b3ac41c55d6d8f9edd"],
-            ["@djencks/asciidoctor-antora-indexer", "npm:0.0.5"],
+            ["@djencks/asciidoctor-antora-indexer", "npm:0.0.6"],
+            ["@djencks/asciidoctor-jsonpath", "npm:0.0.1"],
             ["@zregvart/cargo-deadlinks", "github-release:deadlinks/cargo-deadlinks:0.6.1/deadlinks-linux"],
             ["antora-ui-camel", "workspace:antora-ui-camel"],
             ["del", "npm:6.0.0"],
@@ -5300,6 +5314,13 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         }]
       ]],
       ["esprima", [
+        ["npm:1.2.2", {
+          "packageLocation": "./.yarn/cache/esprima-npm-1.2.2-506b351d14-8cb4d8a7f6.zip/node_modules/esprima/",
+          "packageDependencies": [
+            ["esprima", "npm:1.2.2"]
+          ],
+          "linkType": "HARD",
+        }],
         ["npm:4.0.1", {
           "packageLocation": "./.yarn/cache/esprima-npm-4.0.1-1084e98778-5df45a3d9c.zip/node_modules/esprima/",
           "packageDependencies": [
@@ -8738,6 +8759,18 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageLocation": "./.yarn/cache/jsonparse-npm-1.3.1-b6fde74828-6669acd7b3.zip/node_modules/jsonparse/",
           "packageDependencies": [
             ["jsonparse", "npm:1.3.1"]
+          ],
+          "linkType": "HARD",
+        }]
+      ]],
+      ["jsonpath", [
+        ["npm:1.0.2", {
+          "packageLocation": "./.yarn/unplugged/jsonpath-npm-1.0.2-15a9845dca/node_modules/jsonpath/",
+          "packageDependencies": [
+            ["jsonpath", "npm:1.0.2"],
+            ["esprima", "npm:1.2.2"],
+            ["static-eval", "npm:2.0.2"],
+            ["underscore", "npm:1.7.0"]
           ],
           "linkType": "HARD",
         }]
@@ -13548,6 +13581,14 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         }]
       ]],
       ["static-eval", [
+        ["npm:2.0.2", {
+          "packageLocation": "./.yarn/cache/static-eval-npm-2.0.2-047eda8cb5-58a3e85b2e.zip/node_modules/static-eval/",
+          "packageDependencies": [
+            ["static-eval", "npm:2.0.2"],
+            ["escodegen", "npm:1.14.3"]
+          ],
+          "linkType": "HARD",
+        }],
         ["npm:2.1.0", {
           "packageLocation": "./.yarn/cache/static-eval-npm-2.1.0-d3c8eda113-6c8b0dfdba.zip/node_modules/static-eval/",
           "packageDependencies": [
@@ -14803,6 +14844,15 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["get-assigned-identifiers", "npm:1.2.0"],
             ["simple-concat", "npm:1.0.0"],
             ["xtend", "npm:4.0.2"]
+          ],
+          "linkType": "HARD",
+        }]
+      ]],
+      ["underscore", [
+        ["npm:1.7.0", {
+          "packageLocation": "./.yarn/cache/underscore-npm-1.7.0-7d19d583a5-ba1d3ceeb6.zip/node_modules/underscore/",
+          "packageDependencies": [
+            ["underscore", "npm:1.7.0"]
           ],
           "linkType": "HARD",
         }]

.yarn/build-state.yml
@@ -17,6 +17,10 @@
 "c1e70a98c194359902d396160440b8d6aafc026faec0b483b98d24dac1a2756f6ebdb0d5b12c609339320ad38e22580c8c8b6e70edfc9c5019a30d10094fd1fb":
   e9c1563e2373204e8fc35a861b2b83ea17885f94bb6ce5707801a65373fe681d3aa547beefe41d5a3736ec8d2b989837231ccad01cd13755ac9d11b123a2b003
 
+# jsonpath@npm:1.0.2
+"15a9845dcac098aea0fce6305149a650048254230879c676eaa1f0c5c7d0a5e7f876de3520b3cdd4f066307a91b2809cc494aaab558fa55d7a4e34efae876ce0":
+  18321b920a9e6675bce6044913c34b5d05f005601416d30db27b9b5022628cf0addc3c7d95a60a74e365ed1715d4842339e95ca4878f2f8bdd5a16b2041423db
+
 # optipng-bin@npm:6.0.0
 "3aa5f04a3e5704a556a7f98fdddf5ad260b55ca04a2b8cd3d448861d0bd4778bbc092bf0bd558eba02998faceda73052710fa3124f1940098d2ee8bd93474567":
   0e1ffb3d99b4fe40dd026f2716e011b42c9100563c48a4fe7ca1f57114b7254b3f853b7baf8f1c4f33dc5e253dc26f852533372b0af44d52f68d046a4ccce24a

commit cec20661c6268bb370f21aacf36b6da55e4b3e5b

fix: nav-explore not showing anything but current
A bug was introduced in fb75497471e8975238ed6114e3550e01f02c533f, in
effort to remove the Camel Quarkus Examples from the nav-explore planel
an JavaScript object was converted to an Array, and when rendered only
the first item was considered. This fixes this issue by making sure that
the structure of the data doesn't change, i.e. given an object it will
output an object when filtering out data for the nav-explore panel.

antora-ui-camel/src/helpers/exclude.js
@@ -1,7 +1,3 @@
 'use strict'
 
-module.exports = (elements, del) => {
-  const ary = Object.entries(elements)
-  const idx = ary.indexOf(del)
-  return ary.splice(idx, 1)
-}
+module.exports = (elements, del) => Object.fromEntries(Object.entries(elements).filter(([k, v]) => v.name !== del))

antora-ui-camel/src/partials/nav-explore.hbs
@@ -6,7 +6,7 @@
   </div>
   {{/if}}
   <ul class="components">
-    {{#each (exclude (order_components site.components) 'Camel Quarkus examples')}}
+    {{#each (exclude (order_components site.components) 'camel-quarkus-examples')}}
     <li class="component{{#if (eq this @root.page.component)}} is-current{{/if}}">
       <span class="title">{{{./title}}}</span>
       <ul class="versions">