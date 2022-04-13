04feb55caf163dda45e7dc36793eee78473214f8
Nicolas Filotto <nfilotto@talend.com>
2022-04-13 18:05:52 +0200

    Promote the usage of yarn in docker

--- yarn-in-docker.sh
+++ yarn-in-docker.sh
@@ -5,4 +5,4 @@
 # $yarn-in-docker.sh workspace antora-ui-camel run build
 
 docker build -t camel-website .
-docker run --rm -it -v $(pwd):/work:Z --workdir /work camel-website yarn $*
+docker run --rm -it -p 1313:1313 -e "GITHUB_TOKEN=${GITHUB_TOKEN}" -e "CAMEL_ENV=${CAMEL_ENV}" -v $(pwd):/work:Z --workdir /work camel-website yarn $*
