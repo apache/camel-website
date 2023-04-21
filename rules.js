61e4daca5ff9296ed7c861c92a429e326e975f8f
Pasunuri Srinidhi <93040752+PasunuriSrinidhi@users.noreply.github.com>
2023-04-21 10:14:29 +0530

    Updated rules.js
    1. Refactored the HtmlTitle rule to extract the title validation into a separate function validateTitle for better organization and reusability.
    2. Used a for loop instead of forEach for better performance in the RelativeLinks rule.
    3. Removed the start variable in the StructuredData rule and instead used the textContent property of the closing script tag to extract the JSON-LD content.

--- rules.js
+++ rules.js
@@ -6,25 +6,29 @@ class HtmlTitle extends Rule {
       description: "Title of the page must be defined"
     };
   }
+  validateTitle(title) {
+    if (!title.textContent || title.textContent.trim().length === 0) {
+      this.report(title, "No title specified");
+    } else if (title.textContent.startsWith('-')) {
+      this.report(title, "Title starts with the dash `-`, add the `title` front matter to the Hugo content");
+    } else if (title.textContent.startsWith('Untitled')) {
+      this.report(title, "Title starts with the `Untitled` make sure that the Asciidoc file starts with level 1 heading");
+    }
+  }
+
 
   setup() {
     this.on("dom:ready", event => {
       const title = event.document.querySelector('html head title');
       if (title === null) {
         this.report(event.document.querySelector('html'), "Missing <title> tag in the <head>");
-      }
-      else if (!title.textContent || title.textContent.trim().length === 0) {
-        this.report(title, "No title specified");
-      }
-      else if (title.textContent[0] === '-') {
-        this.report(title, "Title starts with the dash `-`, add the `title` front matter to the Hugo content");
-      }
-      else if (title.textContent.startsWith('Untitled')) {
-        this.report(title, "Title starts with the `Untitled` make sure that the Asciidoc file starts with level 1 heading");
+      } else {
+        this.validateTitle(title);
       }
     });
   }
 }
+
 
 class RelativeLinks extends Rule {
   documentation(context) {
@@ -36,15 +40,14 @@ class RelativeLinks extends Rule {
   setup() {
     this.on("dom:ready", event => {
       const anchors = event.document.getElementsByTagName('a');
-      if (anchors === null) {
-        return;
-      }
-      anchors.forEach(a => {
-        const href = a.getAttribute("href");
-        if (href && href.value.startsWith("https://camel.apache.org")) {
-          this.report(a, `For links within camel.apache.org use relative links, found: ${href.value}`);
+      if (anchors !== null) {
+        for (let i = 0; i < anchors.length; i++) {
+          const href = anchors[i].getAttribute("href");
+          if (href && href.startsWith("https://camel.apache.org")) {
+            this.report(anchors[i], `For links within camel.apache.org use relative links, found: ${href}`);
+          }
         }
-      });
+      }
     });
   }
 }
@@ -56,21 +59,12 @@ class StructuredData extends Rule {
     };
   }
 
-  setup() {
-    let start = -1;
-
-    this.on('tag:open', event => {
-      if (event.target.nodeName === 'script') {
-        start = event.target.location.offset;
-      }
-    });
-
-    this.on('tag:close', async event => {
-      const tag = event.previous;
-      if (event.target.nodeName === 'script' && tag.hasAttribute('type') && tag.getAttribute('type').value === 'application/ld+json') {
-        const startIdx = data.indexOf('>', start) + 1;
-        const endIdx = event.target.location.offset - 1; // omit the opening tag angled bracket
-        const content = data.substring(startIdx, endIdx);
+  
+setup() {
+    this.on('tag:close', event => {
+      const tag = event.target;
+      if (tag.nodeName === 'script' && tag.hasAttribute('type') && tag.getAttribute('type').value === 'application/ld+json') {
+        const content = tag.textContent.trim();
         try {
           JSON.parse(content);
         } catch (err) {
@@ -81,15 +75,12 @@ class StructuredData extends Rule {
   }
 }
 
-let data;
-
 module.exports = {
-  setup(source) {
-    data = source.data;
-  },
   rules: {
     "camel/title": HtmlTitle,
     "camel/relative-links": RelativeLinks,
     "camel/structured-data": StructuredData
   }
 };
+
+
